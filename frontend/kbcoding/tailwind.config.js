909661d5615447e2c4a727fc608f695ba1d008f1
baconkev000 <kevinbaconweb@gmail.com>
2023-10-31 14:50:19 -0600

    add nav component

--- frontend/kbcoding/tailwind.config.js
+++ frontend/kbcoding/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dgrey': '#161616',
+        'mgrey': '#222222',
+        'bgrey': '#2D2D2D',
+        'lgrey': '#ffffff',
+        'a-green': '#7DE3A8',
+        'a-yellow': '#EED17A',
+        'a-purple': '#DEB8F6',
+      }
+    },
   },
   plugins: [],
 }
